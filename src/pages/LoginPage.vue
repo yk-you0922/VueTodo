1a85093d5891b20162b944612a97dd70bebb914e
yk-you0922 <wkgtyou0922@gmail.com>
2022-01-30 14:29:32 +0900

    axiosによる簡易ログイン機能追加

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,7 +1,34 @@
 <template>
   <div id="login-wrapper">
-    <h1 style="color: #fff">Login Page</h1>
-    <button @click="onClickCallUsers">users</button>
+    <div class="login-title">
+      <h3>Login</h3>
+    </div>
+    <Card class="login-card">
+      <div>
+        <form>
+          <div class="login-form">
+            <input 
+              placeholder="Email" 
+              class="login-input"
+              v-model="state.email"
+            />
+          </div>
+          <div class="login-form">
+            <input
+              placeholder="UserName"
+              class="login-input"
+              v-model="state.username"
+            />
+          </div>
+          <button 
+            class="login-btn"
+            @click="onClickLogin($event, state)"
+          >
+            ログイン
+          </button>
+        </form>
+      </div>
+    </Card>
   </div><!-- #login-wrapper -->
 </template>
 
@@ -12,39 +39,104 @@ import {
 } from 'vue';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { User } from '../types/User';
+import Card from '@/components/Card.vue';
+import { useRouter } from 'vue-router';
+
+interface State {
+  appTitle: String;
+  email: String;
+  username: String
+}
 
 export default defineComponent ({
   name: 'LoginPage',
   components: {
     // コンポーネント化したものをここで宣言
+    Card
   },
   setup() {
     // 参照するオブジェクト
-    const state = reactive({
+    const state = reactive<State>({
       appTitle: 'VueTodo',
+      email: '',
+      username: ''
     })
 
+    // vue-routerによる遷移
+    const router = useRouter();
+
+    // 叩き先のAPIURLなどの設定値
     const target: AxiosRequestConfig = {
       url: "https://jsonplaceholder.typicode.com/users",
       method: "GET"
     }
-    const onClickCallUsers = () => {
+    const onClickLogin = (event: MouseEvent, state: State) => {
+      event.preventDefault();
       axios(target)
         .then((response: AxiosResponse<Array<User>>) => {
-          const { data, status } = response;
-          console.log(response);
-          console.log(data, status);
+          response.data.forEach((resUser) => {
+            // 入力値とJSONの返却地が一致する場合、ログイン処理
+            if (resUser.email === state.email && resUser.username === state.username) {
+              // /todoへ遷移させる
+              router.push('/todo')
+            }
+          })
+        })
+        .catch((ex) => {
+          // エラー処理
+          if (axios.isAxiosError(ex) && ex.response && ex.response.status === 400) {
+            console.error("400 Error");
+            console.error(ex.message);
+          }
         })
     }
 
     return {
       state,
-      onClickCallUsers,
+      onClickLogin,
     }
   }
 }) // export default defineComponent
 </script>
 
 <style scoped>
+  #login-wrapper {
+    text-align: center;
+  }
 
+  .login-title {
+    color: #eee;
+    font-size: 40px;
+  }
+
+  .login-card {
+    margin: 0 auto;
+  }
+
+  /* フォーム+ボタン */
+  .login-form {
+    margin-top: 20px;
+  }
+
+  .login-form input {
+    width: 80%;
+    padding: 10px;
+    border-radius: 8px;
+  }
+
+  .login-btn {
+    width: 20%;
+    padding: 10px;
+    margin-top: 20px;
+    background-color: teal;
+    color: #eee;
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: 15px;
+  }
+
+  .login-btn:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
 </style>
